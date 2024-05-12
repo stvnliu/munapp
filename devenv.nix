12cb67f44d52ef8e8cabcd44ba7417b6edc62dce
Zhongheng Liu <z.liu@outlook.com.gr>
2024-05-12 10:48:30 +0300

    chore: update devenv declaration to use commitizen commit linting

--- devenv.nix
+++ devenv.nix
@@ -1,38 +1,40 @@
 { pkgs, lib, config, inputs, ... }:
 
 {
-  # https://devenv.sh/basics/
-  env.GREET = "devenv";
+# https://devenv.sh/basics/
+	env.GREET = "devenv";
 
-  # https://devenv.sh/packages/
-  packages = [ pkgs.git ];
+# https://devenv.sh/packages/
+	packages = [ pkgs.git ];
 
-  # https://devenv.sh/scripts/
-  scripts.hello.exec = "echo hello from $GREET";
+# https://devenv.sh/scripts/
+	scripts.hello.exec = "echo hello from $GREET";
 
-  enterShell = ''
-    hello
-    git --version
-  '';
+	enterShell = ''
+		hello
+		git --version
+		'';
 
-  # https://devenv.sh/tests/
-  enterTest = ''
-    echo "Running tests"
-    git --version | grep "2.42.0"
-  '';
+# https://devenv.sh/tests/
+	enterTest = ''
+		echo "Running tests"
+		git --version | grep "2.42.0"
+		'';
 
-  # https://devenv.sh/services/
-  # services.postgres.enable = true;
+# https://devenv.sh/services/
+# services.postgres.enable = true;
 
-  # https://devenv.sh/languages/
-  languages.nix.enable = true;
-  languages.rust.enable = true;
+# https://devenv.sh/languages/
+	languages.nix.enable = true;
+	languages.rust.enable = true;
 
-  # https://devenv.sh/pre-commit-hooks/
-  # pre-commit.hooks.shellcheck.enable = true;
+# https://devenv.sh/pre-commit-hooks/
+	pre-commit.hooks = {
+		commitizen.enable = true;
+		clippy.enable = true;
+	};
+# https://devenv.sh/processes/
+# processes.ping.exec = "ping example.com";
 
-  # https://devenv.sh/processes/
-  # processes.ping.exec = "ping example.com";
-
-  # See full reference at https://devenv.sh/reference/options/
+# See full reference at https://devenv.sh/reference/options/
 }
