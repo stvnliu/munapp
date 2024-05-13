fad10250d03dd730c2486d18abddc98d38f949eb
Zhongheng Liu <z.liu@outlook.com.gr>
2024-05-13 11:14:03 +0300

    chore: update devenv configuration

--- devenv.nix
+++ devenv.nix
@@ -3,40 +3,44 @@
 {
 # https://devenv.sh/basics/
   env = {
-		GREET = "devenv";
-		CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER = "${pkgs.llvmPackages.clangUseLLVM}/bin/clang";
-		#CARGO_ENCODED_RUSTFLAGS = "-Clink-arg=-fuse-ld=${pkgs.mold}/bin/mold";
-		LD_LIBRARY_PATH = "${pkgs.ncurses}/lib:$LD_LIBRARY_PATH";
-	};
+    GREET = "devenv";
+    CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER = "${pkgs.llvmPackages.clangUseLLVM}/bin/clang";
+#CARGO_ENCODED_RUSTFLAGS = "-Clink-arg=-fuse-ld=${pkgs.mold}/bin/mold";
+    LD_LIBRARY_PATH = "${pkgs.ncurses}/lib:$LD_LIBRARY_PATH";
+  };
 # https://devenv.sh/packages/
-	packages = [ pkgs.git pkgs.ncurses];
+  packages = [
+    pkgs.git
+    pkgs.ncurses
+  ];
 
 # https://devenv.sh/scripts/
-	scripts.hello.exec = "echo hello from $GREET";
+#scripts.hello.exec = "echo hello from $GREET";
   cachix.enable = false;
-	enterShell = ''
-		hello
-		git --version
-		'';
+  enterShell = ''
+    git --version
+    rustc --version
+    cargo version
+    '';
 
 # https://devenv.sh/tests/
-	enterTest = ''
-		echo "Running tests"
-		git --version | grep "2.42.0"
-		'';
+  enterTest = ''
+    echo "Running tests"
+    git --version | grep "2.42.0"
+    '';
 
 # https://devenv.sh/services/
 # services.postgres.enable = true;
 
 # https://devenv.sh/languages/
-	languages.nix.enable = true;
-	languages.rust.enable = true;
+  languages.nix.enable = true;
+  languages.rust.enable = true;
 
 # https://devenv.sh/pre-commit-hooks/
-	pre-commit.hooks = {
-		commitizen.enable = true;
-		clippy.enable = true;
-	};
+  pre-commit.hooks = {
+    commitizen.enable = true;
+    clippy.enable = true;
+  };
 # https://devenv.sh/processes/
 # processes.ping.exec = "ping example.com";
 
